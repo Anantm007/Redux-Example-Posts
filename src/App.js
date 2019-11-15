a80540bf3fa64fb4d5733e6a771d644c9ba1d8d8
Anant Mathur <anant.mathur007@gmail.com>
2019-11-15 18:26:33 +0530

    Install redux

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
+      <hr />
       <Posts />
     </div>
   );
